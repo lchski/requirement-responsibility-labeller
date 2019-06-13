420abdbf22e6f6b380e4ae839e084a224565e01f
Lucas Cherkewski <lucas.cherkewski@cds-snc.ca>
2019-06-13 13:49:30 -0400

    first pass at "find responsible rows" function

--- analyse.R
+++ analyse.R
@@ -10,5 +10,15 @@ responsibles[,1]
 responsibles %>% slice(6)
 
 find_responsible_requirements <- function(requirements, responsibles, index) {
+  # 1. Select a row, noting its index in `responsibles`
+  # 2. Select all rows between row[index].`row` and row[index + 1].`row`
+  # 3. Assign those rows to row[index]
   
+  responsible_row <- responsibles %>% slice(index) %>% select(row) %>% pull()
+  next_responsible_row <- responsibles %>% slice(index + 1) %>% select(row) %>% pull()
+  next_responsible_row <- next_responsible_row - 1
+
+  list(requirements %>% slice(responsible_row:next_responsible_row))
 }
+
+find_responsible_requirements(requirements_with_tokens, responsibles, 1)
