5580b36f90f1661e166b3919325da622c5c914aa
Lucas Cherkewski <lucas@ecustom.ca>
2019-07-31 20:19:07 -0400

    woo, a bunch of responsible matching that's sensible-r

--- analyse.R
+++ analyse.R
@@ -5,11 +5,31 @@ responsibles <- requirements_with_tokens %>%
 
 responsibles %>% write_csv("data/out/responsibles.csv")
 
+responsible_signals <- read_csv("data/responsible_signals.csv")
+
+responsible_signals <- paste(collapse = "|", responsible_actors %>%
+  mutate(actor = case_when(
+    pluralization == "singular" ~ paste0(actor, " is"),
+    pluralization == "plural" ~ paste0(actor, " are"),
+    TRUE ~ actor
+  )) %>%
+  mutate(actor = case_when(
+    is.na(suffix) ~ paste(actor, "responsible"),
+    TRUE ~ paste(actor, suffix)
+  )) %>%
+  pull(actor)
+)
+
+is_clause_describing_responsibility <- function(clauses) {
+  str_detect(clauses, regex(responsible_signals, ignore_case = TRUE))
+}
+
 requirements_tagged_with_responsibles <- requirements_with_tokens %>%
   mutate(responsible_clause = case_when(
-    str_detect(text, "responsible") ~ row,
+    is_clause_describing_responsibility(text) ~ row,
     TRUE ~ NA_integer_
   )) %>%
   fill(responsible_clause)
 
-requirements_tagged_with_responsibles %>% write_csv("data/out/requirements_tagged_with_responsibles.csv")
+requirements_tagged_with_responsibles %>%
+  write_csv("data/out/requirements_tagged_with_responsibles.csv")
