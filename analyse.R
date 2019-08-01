9b9cb118f1b8884f3e944378095aeda73ea68b28
Lucas Cherkewski <lucas@ecustom.ca>
2019-07-31 21:00:11 -0400

    remove cruft, document

--- analyse.R
+++ analyse.R
@@ -1,5 +1,13 @@
 source("load.R")
 
+## Pull in `data/responsible_signals.csv`:
+##   - actor: a form of an actor name
+##   - actor_id: the ID of that actor's standardized name
+##   - pluralization: whether this form of the name is singular or plural
+##   - suffix: whether to append a custom suffix
+## If the `actor` is has a value for `pluralization`, assume it's “is responsible” or “are responsible” accordingly.
+## Else, use the custom suffix.
+## e.g. Deputy heads/plural becomes “Deputy heads are responsible”
 responsible_signals <- read_csv("data/responsible_signals.csv") %>%
   mutate(actor_pluralized = case_when(
     pluralization == "singular" ~ paste0(actor, " is"),
@@ -12,6 +20,7 @@ responsible_signals <- read_csv("data/responsible_signals.csv") %>%
   )) %>%
   left_join(responsible_actors, by = c("actor_id" = "id"))
 
+## Helper function to concatenate the suffixed actor forms into one long regex for `str_detect`.
 is_clause_describing_responsibility <- function(clauses) {
   str_detect(
     string = clauses,
@@ -22,8 +31,13 @@ is_clause_describing_responsibility <- function(clauses) {
   )
 }
 
+## Tag the requirements!
+##   1. Identify whether a clause describes responsibility. If so:
+##     - set `responsible_clause` to the clause’s `row`
+##     - store the string that signalled responsibility
+##   2. Fill the values down (each non-responsibility clause inherits the previous’s responsible actor).
+##   3. Convert the strings signalling responsibility to their standardized actor names.
 requirements_tagged_with_responsibles <- requirements_with_tokens %>%
-  group_by(id) %>%
   mutate(
     responsible_clause = case_when(
       is_clause_describing_responsibility(text) ~ row,
--- load.R
+++ load.R
@@ -27,17 +27,3 @@ requirements_with_tokens <- tibble(id = policies_raw$doc_id, text = policies_raw
   select(id, row, title, text)
 
 requirements_with_tokens %>% write_csv("data/out/requirements_with_tokens.csv")
-
-requirements <- tibble(id = policies_raw$doc_id, text = policies_raw$text) %>%
-  mutate(text = replace_html(text)) %>%
-  mutate(text = str_split(text, "   ")) %>%
-  unnest() %>%
-  mutate(text = trimws(text)) %>%
-  filter(text != "") %>%
-  mutate(id = as.integer(str_remove(id, ".xml.txt"))) %>%
-  left_join(policy_titles) %>%
-  group_by(id) %>%
-  mutate(row = row_number()) %>%
-  select(id, row, title, text)
-
-requirements %>% write_csv("data/out/requirements.csv")
