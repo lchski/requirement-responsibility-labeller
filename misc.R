9c5ecc21cb20866e825a5bebbb12c8a0079c2749
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-21 19:12:23 -0500

    bring in almost every responsible signal

--- misc.R
+++ misc.R
@@ -1,3 +1,5 @@
 ### Review for requirements without responsibles (some may remain, that's okay!)
 requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% View()
+
+#### Check for those with "responsible" in the text
 requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% filter(str_detect(text, "responsible")) %>% View()
