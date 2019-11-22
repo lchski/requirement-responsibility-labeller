d600f10917c3cb755001e500f5e2660a814b3e67
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-21 19:45:05 -0500

    searching

--- scripts/misc.R
+++ scripts/misc.R
@@ -3,3 +3,8 @@ requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% 
 
 #### Check for those with "responsible" in the text
 requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% filter(str_detect(text, "responsible")) %>% View()
+
+requirements_tagged_with_responsibles %>%
+  filter(str_detect(text, "responsible")) %>%
+  filter(is.na(responsible_actor_standardized)) %>%
+  write_csv("data/out/potential_responsible_clauses.csv")
