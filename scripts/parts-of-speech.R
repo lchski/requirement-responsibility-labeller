30a00077be0036341d9a20e1deac20476cf99655
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-26 21:51:20 -0500

    yayyy working POS tagging

--- scripts/parts-of-speech.R
+++ scripts/parts-of-speech.R
@@ -5,15 +5,24 @@ library(openNLPmodels.en)
 
 library(cleanNLP)
 
-requirements_by_pos <- requirements_with_tokens %>%
-  ungroup() %>%
-  mutate(doc_id = paste0(id,  '-', row))
-
 cnlp_init_udpipe(model_name="english")
 
-annotation <- cnlp_annotate(input = requirements_by_pos)
-annotation
+requirements_by_word_pos <- requirements_with_tokens %>%
+  ungroup() %>%
+  mutate(doc_id = paste0(id,  '-', row)) %>%
+  cnlp_annotate()
 
+requirements_by_word_pos <- requirements_by_word_pos$token %>%
+  separate(doc_id, into = c("id", "row"), remove = FALSE, sep = "-", convert = TRUE)
+
+
+## Requirements to plan!
+requirements_by_word_pos %>%
+  filter(lemma == "plan") %>%
+  filter(upos != "VERB") %>%
+  select(id, row) %>%
+  unique() %>%
+  left_join(requirements_tagged_with_responsibles)
 
 
 
