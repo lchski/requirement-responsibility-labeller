c487e648fee97fc3c272a32b532b8f1d43f18d8f
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-27 09:02:41 -0500

    Better identifying/downloading of policy docs

--- load.R
+++ load.R
@@ -9,7 +9,10 @@ responsible_actors <- read_csv("data/responsible_actors.csv")
 
 policies_raw <- readtext("data/policies/*")
 policies_raw <- tibble(id = policies_raw$doc_id, text = policies_raw$text) %>%
-  mutate(id = as.integer(str_remove(id, ".xml.txt")))
+  mutate(id = str_remove(id, fixed(".xml.txt"))) %>%
+  mutate(id = str_remove(id, fixed("doc-eng.aspx?id="))) %>%
+  mutate(id = str_remove(id, fixed("&section=xml"))) %>%
+  mutate(id = as.integer(id))
 
 policy_titles <- read_csv("data/policies.csv")
 
--- scripts/generate.R
+++ scripts/generate.R
@@ -6,13 +6,24 @@
 #     title: a.innerHTML
 #   };
 # })))
+#
+# Generating JSON (from https://www.tbs-sct.gc.ca/pol/hierarch-eng.aspx)
+#
+# console.log(JSON.stringify(Array.from(document.querySelectorAll('.tv-in')).map(a => {
+# return {
+# 	id: new URL(a.href).search.split('=')[1],
+# 	title: a.innerHTML
+# };
+# })))
 
 ## TODO: Combine alphabetical with hierarchical index?
 
 library(jsonlite)
 
-as_tibble(fromJSON("data/policies.json", flatten = TRUE)) %>%
+as_tibble(fromJSON("data/policies-hierarch.json", flatten = TRUE)) %>%
+  bind_rows(as_tibble(fromJSON("data/policies-az.json", flatten = TRUE))) %>%
   mutate(raw_id = id, id = as.integer(id)) %>%
+  unique() %>%
   write_csv("data/policies.csv")
 
 source("load.R")
@@ -22,3 +33,14 @@ policy_titles %>%
   anti_join(policies_raw) %>%
   filter(! is.na(id)) %>%
   pull(id)
+
+## Policies to download!
+## Download: `xargs -n 1 curl -O < data/out/policies-to-download.txt`
+policy_titles %>%
+  anti_join(policies_raw) %>%
+  filter(! is.na(id)) %>%
+  mutate(url = paste0("https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=", id, "&section=xml")) %>%
+  select(url) %>%
+  write_csv("data/out/policies-to-download.txt")
+
+
