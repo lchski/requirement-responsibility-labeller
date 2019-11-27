66fbd082cf33cf265761d75f3afc481f0fa44353
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-26 23:08:14 -0500

    identify policies to download

--- scripts/generate.R
+++ scripts/generate.R
@@ -1,4 +1,4 @@
-# Generating JSON:
+# Generating JSON (from https://www.tbs-sct.gc.ca/pol/a-z-eng.aspx)
 #
 # console.log(JSON.stringify(Array.from(document.querySelectorAll('[role=main] > section li a')).map(a => {
 #   return {
@@ -7,8 +7,18 @@
 #   };
 # })))
 
+## TODO: Combine alphabetical with hierarchical index?
+
 library(jsonlite)
 
 as_tibble(fromJSON("data/policies.json", flatten = TRUE)) %>%
   mutate(raw_id = id, id = as.integer(id)) %>%
   write_csv("data/policies.csv")
+
+source("load.R")
+
+## Policies to download!
+policy_titles %>%
+  anti_join(policies_raw) %>%
+  filter(! is.na(id)) %>%
+  pull(id)
