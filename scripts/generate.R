ef64d5e62ceb274f6f53d611b04e21a9514066d0
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-11-26 23:04:50 -0500

    rebuild policies list using alphabetical index

--- scripts/generate.R
+++ scripts/generate.R
@@ -1,11 +1,14 @@
 # Generating JSON:
 #
-# console.log(JSON.stringify(Array.from(document.querySelectorAll('.tv-in')).map(a => {
-# return {
-# 	id: new URL(a.href).search.split('=')[1],
-# 	title: a.innerHTML
-# };
+# console.log(JSON.stringify(Array.from(document.querySelectorAll('[role=main] > section li a')).map(a => {
+#   return {
+#     id: new URL(a.href).search.split('=')[1],
+#     title: a.innerHTML
+#   };
 # })))
 
+library(jsonlite)
+
 as_tibble(fromJSON("data/policies.json", flatten = TRUE)) %>%
+  mutate(raw_id = id, id = as.integer(id)) %>%
   write_csv("data/policies.csv")
