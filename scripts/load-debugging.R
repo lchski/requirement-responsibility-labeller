76fdc84f376a2619e182ba81e0acdb46db590939
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-12-03 15:24:01 -0500

    identify a few more delivery policies

--- scripts/load-debugging.R
+++ scripts/load-debugging.R
@@ -8,3 +8,20 @@ policies_raw %>% filter(! id %in% (policies_raw %>%
                                      pull(id))) %>%
   left_join(policy_titles) %>%
   select(-text)
+
+### Review for requirements without responsibles (some may remain, that's okay!)
+requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% View()
+
+#### Check for those with "responsible" in the text
+requirements_tagged_with_responsibles %>% filter(is.na(responsible_clause)) %>% filter(str_detect(text, "responsible")) %>% View()
+
+requirements_tagged_with_responsibles %>%
+  filter(str_detect(text, "responsible")) %>%
+  filter(is.na(responsible_actor_standardized)) %>%
+  write_csv("data/out/potential_responsible_clauses.csv")
+
+requirements_tagged_with_responsibles %>%
+  filter(str_detect(text, "responsible")) %>%
+  filter(! clause_assigns_responsiblity) %>%
+  filter(! is.na(responsible_actor_standardized)) %>%
+  write_csv("data/out/potential_misflagged_responsible_clauses.csv")
