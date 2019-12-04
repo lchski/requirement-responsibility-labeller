942713dee6e718ebf5f29d50acd5f32248de81a1
Lucas Cherkewski <lucas@lucascherkewski.com>
2019-12-03 20:55:15 -0500

    Exclude duped policies

--- load.R
+++ load.R
@@ -14,6 +14,14 @@ policies_raw <- tibble(id = policies_raw$doc_id, text = policies_raw$text) %>%
   mutate(id = str_remove(id, fixed("&section=xml"))) %>%
   mutate(id = as.integer(id))
 
-policy_titles <- read_csv("data/policies.csv")
+policy_titles <- read_csv("data/policies.csv") %>%
+  anti_join( ## Remove policies listed under different names. (Do this here, because we generate `data/policies.csv` semi-automatically.)
+    tribble(
+      ~id, ~title, ~raw_id,
+      32594,"Project Complexity and Risk Assessments, Mandatory Procedures for","32594",
+      32594,"Organizational Project Management Capacity Assessments, Mandatory Procedures for","32594",
+      32593,"Concept Cases for Digital Projects, Mandatory Procedures for","32593"
+    )
+  )
 
 source("scripts/load-requirements.R")
